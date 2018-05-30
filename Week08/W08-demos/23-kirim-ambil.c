54c5348f31d267edf9f4ad3ec84d92ec6e36d42a
Albertus Angga Raharja - 1606918401 <albertus.angga@badak.cs.ui.ac.id>
2018-05-30 19:30:05 +0700

    Update week10

--- Week08/W08-demos/23-kirim-ambil.c
+++ Week08/W08-demos/23-kirim-ambil.c
@@ -16,14 +16,16 @@
  warranty of MERCHANTABILITY or FITNESS 
  FOR A PARTICULAR PURPOSE.
 
- * REV03 Tue Apr 24 09:04:50 WIB 2018
+ * REV07 Wed Apr 25 09:28:14 WIB 2018
  * REV00 Wed Apr 18 19:50:01 WIB 2018
  * START Xxx Xxx XX XX:XX:XX WIB 2013
  */
 
 // DO NOT USE THE SAME SEMAPHORE NAME!!!!
-#define SEM_SYNC  "/semaphore-sync-rms46"
-#define SEM_MUTEX "/semaphore-mutex-rms46"
+// Replace "demo" with your own SSO name.
+#define SEM_SYN_KRAM    "/syn-KRAM-demo"
+#define SEM_SYN_AMKR    "/syn-AMKR-demo"
+#define SEM_MUTEX       "/sm_mutex-demo"
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -34,78 +36,89 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define SEM_PERMS  \
- (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
-#define O_FLAGS (O_CREAT | O_EXCL)
+// Shared Memory: R/W with no name.
 #define PROT    (PROT_READ | PROT_WRITE)
-#define VISIBILITY \
-            (MAP_ANONYMOUS | MAP_SHARED)
+#define VISIBLE (MAP_ANON  | MAP_SHARED)
 
 #define KIRIM 0
 #define AMBIL 1
-#define LOOP  10
+#define LOOP  2
 
 typedef struct {
    int    produk;
    int    turn;
    int    loop;
-   sem_t* mutex;
-   sem_t* sync;
 }  buffer;
 
-// WARNING: NO ERROR CHECK! ///////////
-buffer* persiapan(buffer* buf) {
-   buf = (buffer* ) mmap(NULL, 
-            sizeof(buffer), PROT, 
-            VISIBILITY, 0, 0);
-   buf->produk = 0;
-   buf->turn   = KIRIM;
+// KRAM: Kirim-Ambil; AMKR: Ambil-Kirim
+sem_t*    sync_KRAM;
+sem_t*    sync_AMKR;
+sem_t*    sem_mutex;
+
+// WARNING: NO ERROR CHECK! ////////////
+void persiapan(buffer* buf) {
    buf->loop   = 0;
-   sem_unlink(SEM_SYNC);
-   sem_unlink(SEM_MUTEX);
-   buf->sync  = sem_open(SEM_SYNC, 
-              O_FLAGS, SEM_PERMS, 0);
-   buf->mutex = sem_open(SEM_MUTEX, 
-              O_FLAGS, SEM_PERMS, 1);
-   return buf;
+   buf->produk = 0;
+   buf->turn   = AMBIL;
+   sync_KRAM   = sem_open(SEM_SYN_KRAM, 
+                     O_CREAT, 0600, 0);
+   sync_AMKR   = sem_open(SEM_SYN_AMKR, 
+                     O_CREAT, 0600, 0);
+   sem_mutex   = sem_open(SEM_MUTEX, 
+                     O_CREAT, 0600, 1);
+   printf("PR KIRIMAN AWAL: %d\n",
+                           buf->produk);
 }
 
 void kirim (buffer* buf) {
-   printf("KR KIRIM PID[%d]\n", getpid());
+   printf("KR KIRIM PID[%d]\n",getpid());
+   sem_post(sync_KRAM);
+   sem_wait(sync_AMKR);
+   int krLoop = 0;
+   while (buf->turn != KIRIM)
+      ;
    while (buf->loop < LOOP) {
-      sem_wait(buf->mutex);
+      krLoop++;
+      sem_wait(sem_mutex);
       if (buf->turn == KIRIM) {
-         printf("KR %d\n",++(buf->produk));
          buf->turn = AMBIL;
+         printf("KR %d\n",++(buf->produk));
       }
-      sem_post(buf->mutex);
-      if (buf->loop == 0) 
-         sem_post(buf->sync);
+      sem_post(sem_mutex);
    }
+   wait(NULL);
+   printf("KR LOOPS = %d\n", krLoop);
 }
 
 void ambil (buffer* buf) {
-   printf("AM AMBIL PID[%d]\n", getpid());
-   sem_wait(buf->sync);
-   while (buf->loop < LOOP) {
-      sem_wait(buf->mutex);
+   sem_wait(sync_KRAM);
+   sem_post(sync_AMKR);
+   printf("AM AMBIL PID[%d]\n",getpid());
+   int amLoop = 0;
+   while (buf->loop < LOOP+1) {
+      amLoop++;
+      sem_wait(sem_mutex);
       if(buf->turn == AMBIL) {
-         printf("AM %d\n", buf->produk);
          buf->turn = KIRIM;
+         printf("AM %d\n", buf->produk);
+         buf->loop++;
       }
-      buf->loop++;
-      sem_post(buf->mutex);
+      sem_post(sem_mutex);
    }
+   printf("AM LOOPS = %d\n", amLoop);
 }
 
-// WARNING: NO ERROR CHECK! ///////////
+// WARNING: NO ERROR CHECK! ////////////
 void main(void) {
-   printf("START PID[%d]\n", getpid());
-   buffer* shrbuf=persiapan(shrbuf);
+   printf("STARTING PID[%d]\n", getpid());
+   buffer* shrbuf = mmap(NULL,
+                    sizeof(buffer), PROT, 
+                    VISIBLE, 0, 0);
+   persiapan(shrbuf);
    if (fork()) kirim (shrbuf); //Parent
    else        ambil (shrbuf); //Child
-   wait(NULL);
-   sem_unlink(SEM_SYNC);
+   sem_unlink(SEM_SYN_KRAM);
+   sem_unlink(SEM_SYN_AMKR);
    sem_unlink(SEM_MUTEX);
    printf("STOP PID[%d]\n", getpid());
 }
